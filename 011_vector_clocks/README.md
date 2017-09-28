ffb28c139c1827a5c9cfbd5322b55b2702d63930
Tiago Queiroz <contato@tiago.eti.br>
2017-09-28 23:59:59 +0200

    Day 011

---
+++ 011_vector_clocks/README.md
@@ -0,0 +1,7 @@
+Vector Clocks
+=============
+
+Bits of a vector clock implemented as part of the meetup organised by [Women Who Go Berlin](https://www.meetup.com/Women-Who-Go-Berlin/events/242953531/)
+
+
+TODO: Write a proper README, organise the code and the tests
---
+++ 011_vector_clocks/main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("TODO: Write README, organise the code/tests")
+}
+
+// func (t Timestamp) HappensBefore(r Timestamp) int {
+// 	if reflect.DeepEqual(t.vc, r.vc) {
+// 		return Equals
+// 	}
+
+// 	var greatThan, lessThan bool
+// 	var ts, rs int
+
+// 	for i := 0; i < len(t.vc); i++ {
+
+// 		if t.vc[i] > r.vc[i] {
+// 			greatThan = true
+// 		} else if t.vc[i] < r.vc[i] {
+// 			lessThan = true
+// 		}
+
+// 		ts += t.vc[i]
+// 		rs += r.vc[i]
+// 	}
+
+// 	if greatThan && lessThan {
+// 		if t.resolver != nil {
+// 			return t.resolver.Resolve(t, r)
+// 		}
+// 		return NotComparable
+// 	}
+
+// 	if ts < rs {
+// 		return HappensBefore
+// 	}
+
+// 	return HappensAfter
+// }
+
+// // Merge happens when actors in 2 different timelines communicate, for example when a message with timestamp is recieved.
+// // What is reuired:
+// // - update each element in the vector to be max(local, received)
+// // - increment the logical clock value representing the current node in the vector
+// // r remote timeline, recieved as part of a message
+// func (t Timestamp) Merge(r Timestamp) {
+// 	for i := 0; i < len(t.vc); i++ {
+// 		if r.vc[i] > t.vc[i] {
+// 			t.vc[i] = r.vc[i]
+// 		}
+// 	}
+// 	t.Tick()
+// }
+
+// func (r MajorityResolver) Resolve(t Timestamp, o Timestamp) int {
+// 	fmt.Print("RESOLVER")
+// 	max := func(v []int) int {
+// 		m := v[0]
+// 		for i := 1; i < len(v); i++ {
+// 			if m < v[i] {
+// 				m = v[i]
+// 			}
+// 		}
+// 		return m
+// 	}
+
+// 	tm := max(t.vc)
+// 	om := max(o.vc)
+
+// 	if tm > om {
+// 		return HappensAfter
+// 	} else if om > tm {
+// 		return HappensBefore
+// 	}
+
+// 	return Equals
+// }
